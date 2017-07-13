064cc6e06ace33f3dd2f2a6c7ae33ba0248a3234
Ryan Spangler <ryan.spangler@gmail.com>
2017-07-12 23:49:02 -0700

    new protograph in action

--- src/protograph/template.clj
+++ src/protograph/template.clj
@@ -2,7 +2,9 @@
   (:require
    [clojure.string :as string]
    [taoensso.timbre :as log]
+   [selmer.filters :as filters]
    [selmer.parser :as template]
+   [cheshire.core :as json]
    [yaml.core :as yaml]))
 
 (def defaults
@@ -25,10 +27,23 @@
 
 (defn splice-maps
   [before splices context]
+  (log/info before splices context)
   (reduce
    (fn [after splice]
-     (merge after (get context splice)))
+     (merge after (get context (keyword splice))))
    before splices))
+
+(defn parse-unit
+  [u]
+  (try
+    (Integer/parseInt u)
+    (catch Exception e
+      (keyword u))))
+
+(defn parse-index
+  [index]
+  (let [parts (string/split index #"\.")]
+    (map parse-unit parts)))
 
 (def edge-fields
   {:gid "({{from}})--{{label}}->({{to}})"})
@@ -37,37 +52,59 @@
   {})
 
 (defn process-entity
-  [top-level fields {:keys [splice properties] :as directive} entity]
+  [top-level fields {:keys [properties splice filter] :as directive} entity]
   (let [core (select-keys directive top-level)
         top (evaluate-map core entity)
         properties (evaluate-map properties entity)
-        out (splice-maps top splice entity)
-        onto (evaluate-map fields out)]
-    (assoc (merge onto out) :properties properties)))
+        out (splice-maps properties splice entity)
+        merged (if (:merge directive)
+                 (merge entity out)
+                 out)
+        slim (apply dissoc merged (concat splice filter))
+        onto (evaluate-map fields top)]
+    [(assoc (merge onto top) :properties slim)]))
+
+(defn process-index
+  [top-level fields {:keys [index] :as directive} entity]
+  (if (empty? index)
+    (process-entity top-level fields directive entity)
+    (let [path (parse-index index)
+          series (get-in entity path)]
+      (map
+       (comp
+        (partial process-entity top-level fields directive)
+        (partial assoc entity :_index))
+       series))))
 
 (def process-edge
   (partial
-   process-entity
-   [:from-label :from :label :to-label :to]
+   process-index
+   [:fromLabel :from :label :toLabel :to]
    edge-fields))
 
 (def process-vertex
   (partial
-   process-entity
+   process-index
    [:label :gid]
    vertex-fields))
 
-(defn process-entity
-  [{:keys [out] :as directive} message]
-  (condp = (string/lower-case out)
-    "vertex" (process-vertex directive message)
-    "edge" (process-edge directive message)))
+(defn process-directive
+  [{:keys [nodes edges] :as protonode} message]
+  {:nodes (mapcat #(process-vertex % message) nodes)
+   :edges (mapcat #(process-edge % message) edges)})
 
 (defn process-message
   [protograph message]
   (let [label (get message :_label)
-        proto (get protograph label)]
-    (mapv
-     (fn [directive]
-       (process-entity directive message))
-     proto)))
+        directive (get protograph label)]
+    (process-directive directive message)))
+
+(filters/add-filter! :split (fn [s d] (string/split s (re-pattern d))))
+
+(defn load-protograph
+  [path]
+  (let [raw (yaml/parse-string (slurp path))]
+    (reduce
+     (fn [protograph spec]
+       (assoc protograph (:label spec) (select-keys spec [:nodes :edges])))
+     {} raw)))
