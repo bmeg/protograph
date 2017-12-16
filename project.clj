3cf04711c110ce6812b31d845121992f53fb0f67
Ryan Spangler <ryan.spangler@gmail.com>
2017-12-15 16:29:01 -0800

    version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject protograph "0.0.11"
+(defproject protograph "0.0.12"
   :description "tranform a stream of messages into a graph"
   :url "http://github.com/bmeg/protograph"
   :license {:name "Eclipse Public License"
