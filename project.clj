d67f14e96b852473b0565bd679d2b3e6cf2984d9
Ryan Spangler <ryan.spangler@gmail.com>
2018-01-11 15:32:00 -0800

    not emitting _label in output data

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject protograph "0.0.15"
+(defproject protograph "0.0.16"
   :description "tranform a stream of messages into a graph"
   :url "http://github.com/bmeg/protograph"
   :license {:name "Eclipse Public License"
